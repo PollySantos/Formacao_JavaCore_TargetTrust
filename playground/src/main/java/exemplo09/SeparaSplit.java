2bf21ec53fc606dcd828d2c11a640386e1a9bd5c
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2022-06-09 21:53:06 -0300

    exercicio 09

---
+++ playground/src/main/java/exemplo09/SeparaSplit.java
@@ -0,0 +1,31 @@
+package exemplo09;
+
+/*
+ * Split: Faça um método que recebe uma String e retorna cada palavra da sentença em uma linha.
+Entrada: "A pressa é a inimiga da perfeição."
+Saída: "A
+pressa
+é
+a
+inimiga
+da
+perfeição."
+ */
+
+public class SeparaSplit {
+
+    public static void main(String[] args) {
+        String nome = "A pressa é inimiga da perfeição.";
+
+        String[] separacao = nome.split(" ");
+
+        System.out.println(separacao[0]);
+        System.out.println(separacao[1]);
+        System.out.println(separacao[2]);
+        System.out.println(separacao[3]);
+        System.out.println(separacao[4]);
+        System.out.println(separacao[5]);
+
+    }
+
+}
